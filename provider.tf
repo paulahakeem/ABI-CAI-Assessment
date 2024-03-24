ad7ea90ee0d7efe956aca9727b6261a2c480cae4
paulahakeem <paula.hakeem@yahoo.com>
2024-03-24 16:08:01 +0200

    fix errors

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 /* Configure the AWS Provider */
 provider "aws" {
-  access_key = "AKIA6ODU6G2DGUVQDO5B"
+  access_key = var.access_key
   region     = "us-east-1"
-  secret_key = "T6x8WEkVY2AaWDawLE5Rg728I8YRERenaACqvihL"
+  secret_key = var.secret_key
 }
